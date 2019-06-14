863c10eac576fbd4d4cfc6418fa8fbbb1d15d3c5
Marvin Alvarenga <marvinalexz@gmail.com>
2019-06-13 23:55:44 -0600

    Verify user before login and spanish translation

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'Las credenciales no son correctas o usuario no verificado',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
