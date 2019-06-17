e7846303454b69177fd78a9d995d1d901400be08
Oscar Cordero <oscarcordero97@gmail.com>
2019-06-16 21:01:08 -0600

    cambio en rutas

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,32 @@ Auth::routes();
 Route::middleware(['auth'])->group(function () {
     Route::redirect('/home', '/');
 
-    //RUTAS PARA REPORTES GERENCIALES//
+    //RUTAS CH15013
     Route::get('/equipoportipo', 'GerencialController@equipoPorTipo')->name('gerenciales.equipoportipo');
-    Route::get('/info40', 'GerencialController@verInfo40')->name('gerenciales.info40');
-    Route::get('/pdfinfo40', 'GerencialController@pdfInfo40')->name('gerenciales.pdfinfo40');
     Route::get('/repuestosCambiados','GerencialController@repuestosCambiados')->name('gerenciales.repuestosCambiados');
-
-    //RUTAS PARA REPORTES TÁCTICOS//
     Route::get('/mantenimientos','TacticoController@mantenimientosRealizados')->name('tacticos.mantenimientosRealizados');
     Route::get('/licencias','TacticoController@licenciasPorVencer')->name('tacticos.licenciasPorVencer');
 
+
+    
+
+    //RUTAS GONZALO
+    Route::get('/info40', 'GerencialController@verInfo40')->name('gerenciales.info40');
+    Route::get('/pdfinfo40', 'GerencialController@pdfInfo40')->name('gerenciales.pdfinfo40');
+
+
+
+
+    //RUTAS EDWIN
     Route::get('/reportes/mantsxuser', 'GerencialController@getMantsXUser')->name('MantsXUser');
     Route::get('/reportes/equipoDescargado', 'TacticoController@getEquipoDescargado')->name('EquipoDescargado');
     Route::post('/reportes/mantsxuser', 'GerencialController@postMantsXUser')->name('PostMantUsrs');
     Route::post('/reportes/equipoDescargado', 'TacticoController@postEquipoDescargado')->name('PostEquipoDescargado');
 
+
+
+
+    //RUTAS MARVIN
     Route::get('/usuarios', 'UserController@index')->name('usuarios.index');
     Route::get('/usuarios/{usuario}/editar', 'UserController@edit')->name('usuarios.edit');
     Route::post('/usuarios/{usuario}/actualizar', 'UserController@update')->name('usuarios.update');
